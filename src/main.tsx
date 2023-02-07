c8d4c867f5e845d7f78a278e8b76c758011d7d7c
Shaikim River <108586058+RiverXDiamant@users.noreply.github.com>
2023-02-07 10:14:53 -0500

    set up ChakraProvider

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// set up ChakraProvider in root of application
+import {ChakraProvider} from "@chakra-ui/react"
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <ChakraProvider>
     <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
